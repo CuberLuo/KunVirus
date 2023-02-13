1774d1502e095b302658c12470b1788666129c63
CuberLuo <1501858187@qq.com>
2023-02-13 12:10:05 +0800

    添加启动音频

--- app/src/main/java/cn/edu/zjut/kunvirus/MusicService.java
+++ app/src/main/java/cn/edu/zjut/kunvirus/MusicService.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class MusicService extends Service {
+    static MediaPlayer ganma_player;
     static MediaPlayer player;
 
     @Override
@@ -15,9 +16,14 @@ public class MusicService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        ganma_player=MediaPlayer.create(getApplicationContext(), R.raw.ni_gan_ma);
         player = MediaPlayer.create(getApplicationContext(), R.raw.cxk_music);
         player.setLooping(true);
-        player.start();
+        ganma_player.start();
+        ganma_player.setOnCompletionListener(mp -> {
+            player.start();
+        });
+
     }
     @Override
     public void onDestroy() {	//停止服务
